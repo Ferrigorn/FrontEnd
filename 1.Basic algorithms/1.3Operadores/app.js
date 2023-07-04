701f8aa000ddd26fb2163750fdec7808bf5177ad
Ferrigorn <fbabures@gmail.com>
2023-07-04 16:54:19 +0200

    first exercice

---
+++ 1.Basic algorithms/1.3Operadores/app.js
@@ -0,0 +1,42 @@
+/*1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+
+1.2 Divide 10 por 2 y muestra el resultado en un console.
+
+1.3 Muestra mediante un console el resto de dividir 15 por 9.
+
+1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+teniendo dos variables p = 10 y j = 5.
+
+1.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5.*/
+
+
+//1.1
+
+let x = 10;
+let y = 5;
+console.log(x*y)
+
+//1.2
+
+ x /= 2;
+ console.log(x)
+
+
+ //1.3
+
+ let a = 15;
+ let b = 9;
+ console.log(a/b)
+
+ //1.4
+
+ let p = 10;
+ let j = 5;
+ let o = p += j; 
+console.log(o)
+ //1.5
+ let c = 10;
+ let m = 5;
+ let i = c *= m;
+ 
