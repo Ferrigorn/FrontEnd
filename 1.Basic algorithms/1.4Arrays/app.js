f99541b69d7826c23815d92d32fb6a3b0c12048b
Ferrigorn <fbabures@gmail.com>
2023-07-06 17:38:27 +0200

    first exercice

---
+++ 1.Basic algorithms/1.4Arrays/app.js
@@ -0,0 +1,54 @@
+/*1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.2 Cambia el primer elemento de avengers a "IRONMAN"
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.3 console numero de elementos en el array usando la propiedad correcta de Array.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+Muestra en consola el último personaje del array
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+
+1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+1.6 Elimina el segundo elemento del array y muestra el array por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];*/
+
+//1.1 
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+const hulk = avengers[0];
+console.log(hulk);
+
+//1.2
+
+avengers[0] = "IRONMAN";
+console.log(avengers);
+
+//1.3
+
+console.log(avengers.length);
+
+//1.4
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+const ultimo = rickAndMortyCharacters[4];
+console.log(ultimo);
+
+//1.5
+
+rickAndMortyCharacters.push("Lapiz Lopez");
+rickAndMortyCharacters.pop();
+console.log(rickAndMortyCharacters[0], rickAndMortyCharacters[4])
+
+//1.6
+
+
+const rickAndMortyCharacter = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacter.splice(1, 1);
+console.log(rickAndMortyCharacter);
