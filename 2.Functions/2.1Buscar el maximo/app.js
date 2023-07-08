c90d07191bb6ee44e26389a32684f8b3607cf650
Ferrigorn <fbabures@gmail.com>
2023-07-08 10:48:01 +0200

    afegit 2.1

---
+++ 2.Functions/2.1Buscar el maximo/app.js
@@ -0,0 +1,8 @@
+//Completa la funcion que tomando dos numeros como argumento devuelva el mas alto
+
+
+
+function sum(numberOne , numberTwo) {
+  
+  console.log(Math.max([numberOne], [numberTwo]))
+}
