c5375413833c1450c4045b6cb8168fb6911ef3ab
Ferrigorn <fbabures@gmail.com>
2023-07-19 17:36:57 +0200

    Interaccion con el DOM

---
+++ JavaScript/5.DOM/5.1Interaccion/main.js
@@ -0,0 +1,32 @@
+import './style.css'
+
+
+
+// 1.1
+const boton = document.querySelector('.showme');
+console.log(boton);
+
+
+// 1.2
+const h1 = document.querySelector('#pillado');
+console.log(h1);
+
+
+// 1.3
+const parrafo = document.querySelectorAll('p');
+console.log(parrafo);
+
+
+// 1.4
+const pokemon = document.querySelectorAll('.pokemon');
+console.log(pokemon);
+
+
+// 1.5
+const span = document.querySelectorAll('span[data-function="testMe"]');
+console.log(span);
+
+
+
+// 1.6
+console.log(span[2]);
